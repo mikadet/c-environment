ef7df6689d7141c3fc358bb56cc00ec6074190ca
mikadet <95338759+mikadet@users.noreply.github.com>
2021-12-15 10:01:25 +0100

    Update main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+ printf("\n"); //for new line
+
+ // While Loop
+
+ n=1;         //Initialize
+ while(n<=10)   // Condition
+ {
+  printf(" %d \n",n);
+  n++;    // Increment
+ }
+
+
+
+}
